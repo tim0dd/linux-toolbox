3d88446feeb4e956256020425fec4e901134675c
Timo Dittus <timo.daniel.dittus@gmail.com>
2024-01-27 21:55:02 +0100

    Add bash prompt switching

--- konsole-auto-theme-switch/README.md
+++ konsole-auto-theme-switch/README.md
@@ -6,8 +6,9 @@ The script dynamically reads the user's default Konsole profile and updates the 
 
 ## Installation
 - To set up the theme switching schedule, run:
-  ```bash ./install.sh update [light_theme_start_time] [dark_theme_start_time]```
-- Replace [light_theme_start_time] and [dark_theme_start_time] with the hours (in 24-hour format `hh:mm`) you want to switch to the light and dark themes, respectively.
+  ```bash ./install.sh --update <light_start_time> <dark_start_time>```
+- Replace `<light_start_time>` and `<dark_start_time>` with the hours (in 24-hour format `hh:mm`) you want to switch to the light and dark themes, respectively.
+- To also adapt the bash prompt styling, use `bash ./install.sh --update <light_start_time> <dark_start_time> --set_ps1`. **This requires variables DARK_PS1 and LIGHT_PS1 to be predefined in your .bashrc file!**. The script will simply modify your .bashrc by either appending `export PS1=$LIGHT_PS1` or `export PS1=$DARK_PS1` and surrounding the appended lines with identifying comments for automatic removal when switching the theme. **It's recommended to backup your .bashrc file before using this option!**
 
 ## Dependencies
 - cron:
@@ -20,7 +21,4 @@ Using the installation syntax will automatically delete the previously added cro
 
 ## Removal
 Any previously added cronjobs can be removed by running:
-```bash ./install.sh remove```
-
-## Todo
-- Add ability to switch bash prompt (PS1)
+```bash ./install.sh --remove```
--- konsole-auto-theme-switch/install.sh
+++ konsole-auto-theme-switch/install.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-# Paths
+SET_PS1=false
+UPDATE=false
+REMOVE=false
+
 KONSOLE_CONFIG="$HOME/.config/konsolerc"
 KONSOLE_PROFILE_DIR="$HOME/.local/share/konsole"
+BASHRC="$HOME/.bashrc"
+DEFAULT_PROFILE=$(grep 'DefaultProfile=' "$KONSOLE_CONFIG" | cut -d'=' -f2)
+PROFILE_PATH="$KONSOLE_PROFILE_DIR/$DEFAULT_PROFILE"
 
-# Color schemes
 LIGHT_THEME="BlackOnWhite"
 DARK_THEME="WhiteOnBlack"
 
-# Unique identifier for our cron jobs
 CRON_JOB_ID="# KonsoleAutoThemeSwitchID"
 
-# Extract the default profile name
-DEFAULT_PROFILE=$(grep 'DefaultProfile=' "$KONSOLE_CONFIG" | cut -d'=' -f2)
-
-# Construct the profile path
-PROFILE_PATH="$KONSOLE_PROFILE_DIR/$DEFAULT_PROFILE"
-# Function to parse time input (hour:minute) into cron format
 parse_time() {
-    IFS=':' read -ra TIME_PARTS <<< "$1"
-    echo "${TIME_PARTS[1]} ${TIME_PARTS[0]}"
+    IFS=':' read -ra time_parts <<<"$1"
+    echo "${time_parts[1]} ${time_parts[0]}"
 }
 
-# Function to remove existing cron jobs for our script
 remove_existing_cron_jobs() {
     crontab -l | grep -v "$CRON_JOB_ID" | crontab -
 }
 
-# Function to add new cron jobs
+clean_bashrc() {
+    awk '/^# Start KonsoleAutoThemeSwitcher/,/^# End KonsoleAutoThemeSwitcher/ {next} 1' "$BASHRC" >"$BASHRC.tmp" && mv "$BASHRC.tmp" "$BASHRC"
+}
+
 add_cron_jobs() {
     light_time=$(parse_time "$1")
     dark_time=$(parse_time "$2")
@@ -35,31 +35,60 @@ add_cron_jobs() {
     light_command="sed -i 's/^ColorScheme=.*/ColorScheme=$LIGHT_THEME/' $PROFILE_PATH"
     dark_command="sed -i 's/^ColorScheme=.*/ColorScheme=$DARK_THEME/' $PROFILE_PATH"
 
-    (crontab -l 2>/dev/null; echo "$light_time * * * $light_command $CRON_JOB_ID") | crontab -
-    (crontab -l 2>/dev/null; echo "$dark_time * * * $dark_command $CRON_JOB_ID") | crontab -
+    if $SET_PS1; then
+        light_command="$light_command && awk '/^# Start KonsoleAutoThemeSwitcher/,/^# End KonsoleAutoThemeSwitcher/ {next} 1' \"$BASHRC\" >\"$BASHRC.tmp\" && mv \"$BASHRC.tmp\" \"$BASHRC\""
+        dark_command="$dark_command && awk '/^# Start KonsoleAutoThemeSwitcher/,/^# End KonsoleAutoThemeSwitcher/ {next} 1' \"$BASHRC\" >\"$BASHRC.tmp\" && mv \"$BASHRC.tmp\" \"$BASHRC\""
+        light_command="$light_command && echo -e '\\n# Start KonsoleAutoThemeSwitcher\\nexport PS1=\$LIGHT_PS1\\n# End KonsoleAutoThemeSwitcher' >> $BASHRC"
+        dark_command="$dark_command && echo -e '\\n# Start KonsoleAutoThemeSwitcher\\nexport PS1=\$DARK_PS1\\n# End KonsoleAutoThemeSwitcher' >> $BASHRC"
+    fi
+
+    (
+        crontab -l 2>/dev/null
+        echo "$light_time * * * $light_command $CRON_JOB_ID"
+    ) | crontab -
+    (
+        crontab -l 2>/dev/null
+        echo "$dark_time * * * $dark_command $CRON_JOB_ID"
+    ) | crontab -
 }
 
-# Main execution based on command line argument
-case "$1" in
-    update)
-        if [[ -n $2 && -n $3 ]]; then
-            remove_existing_cron_jobs
-            add_cron_jobs "$2" "$3"
-            echo "Cron jobs for theme switching have been updated."
-        else
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+    --set_ps1)
+        SET_PS1=true
+        echo "Enable setting the bash prompt"
+        shift
+        ;;
+    --update)
+        UPDATE=true
+        LIGHT_START_TIME="$2"
+        DARK_START_TIME="$3"
+        if [[ -z $LIGHT_START_TIME || -z $DARK_START_TIME ]]; then
             echo "Error: Missing schedule times for light and dark themes."
-            echo "Usage: $0 update [light_time] [dark_time]"
-            echo "Time format: hour:minute (e.g., 7:30, 19:00)"
+            echo "Usage: $0 --update <light_start_time> <dark_start_time>"
             exit 1
         fi
+        shift 3
         ;;
-    remove)
-        remove_existing_cron_jobs
-        echo "Cron jobs for theme switching have been removed."
+    --remove)
+        REMOVE=true
+        shift
         ;;
     *)
-        echo "Usage: $0 [update [light_time] [dark_time]|remove]"
-        echo "Time format: hour:minute (e.g., 7:30, 19:00)"
+        echo "Unknown option: $1"
+        echo "Usage: $0 [--update <light_start_time> <dark_start_time>] [--set_ps1] [--remove]"
         exit 1
         ;;
-esac
+    esac
+done
+
+if $UPDATE; then
+    remove_existing_cron_jobs
+    clean_bashrc
+    add_cron_jobs "$LIGHT_START_TIME" "$DARK_START_TIME"
+    echo "Cron jobs for theme switching have been updated."
+elif $REMOVE; then
+    remove_existing_cron_jobs
+    clean_bashrc
+    echo "Cron jobs for theme switching and added lines in .bashrc have been removed ."
+fi
